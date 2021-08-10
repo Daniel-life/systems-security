171288a5b2284ae42377da0aea01db1094832a33
Daniel Cheong Jun Jie <83260424+Daniel-life@users.noreply.github.com>
2021-08-10 18:49:30 +0800

    Add files via upload

--- geocoder.py
+++ geocoder.py
@@ -1,20 +1,13 @@
 import requests
 import json
 import socket
-
 hostname = socket.gethostname()
 ip_address = socket.gethostbyname(hostname)
-
-# Your API key, available from your account page
-YOUR_GEOLOCATION_KEY = '3b1399cd7b1947a0a2b8fd3a3f92e285'
-
 # URL to send the request to
-request_url = 'https://ipgeolocation.abstractapi.com/v1/?api_key=' + YOUR_GEOLOCATION_KEY
-# Send request and decode the result
+request_url = 'https://ipgeolocation.abstractapi.com/v1/?api_key=3b1399cd7b1947a0a2b8fd3a3f92e285'
 response = requests.get(request_url)
 result = json.loads(response.content)
 print(result)
-
 info = {
     'city': result['city'],
     'country': result['country'],
