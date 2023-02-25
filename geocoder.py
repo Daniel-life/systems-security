b8ce38e6a33fa5b3568b0ced3e1cccfeb74ed0ee
Daniel Cheong Jun Jie <300157E@mymail.nyp.edu.sg>
2023-02-25 21:16:07 +0800

    modified api url for geocoder

--- geocoder.py
+++ geocoder.py
@@ -4,7 +4,7 @@ import socket
 hostname = socket.gethostname()
 ip_address = socket.gethostbyname(hostname)
 # URL to send the request to
-request_url = 'https://ipgeolocation.abstractapi.com/v1/?api_key=3b1399cd7b1947a0a2b8fd3a3f92e285'
+request_url = 'https://api.ipgeolocation.io/ipgeo?apiKey=264dc73d2ac44acf98985ed04c85b2cf'
 response = requests.get(request_url)
 result = json.loads(response.content)
 print(result)
